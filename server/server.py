378e0aa64383ff7422d396029dd8170b4611b13a
nicholasjhall <njhall@bu.edu>
2022-04-06 22:32:45 -0400

    Add OpenWeather API

--- server/server.py
+++ server/server.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+import os
 
 app = Flask(__name__)
+OPENWEATHER_API_KEY = os.environ.get("OPENWEATHER_API_KEY") 
 
 @app.route("/")
 def hello_world():
     return "<p>This is the CS411 server!</p>"
+
+
+@app.route("/weather")
+def get_weather():
+    response = requests.get("http://api.openweathermap.org/data/2.5/forecast?q=Boston&appid=" + OPENWEATHER_API_KEY)
+    return response.json()
