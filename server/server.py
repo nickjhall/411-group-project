849c6a777e8de95258f1fd39dd8ed3ca0c7d1dde
jzhang0107 <zhang98@bu.edu>
2022-04-07 02:34:38 -0400

    working findRestaurants endpoint

--- server/server.py
+++ server/server.py
@@ -1,7 +1,59 @@
-from flask import Flask
+from audioop import add
+from flask import Flask, request
+from numpy import result_type
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>This is the CS411 server!</p>"
+
+@app.route("/findRestaurants", methods=["POST"])
+def findRestaurants():
+    json = request.get_json()
+
+    term = json["foodType"]
+    location = json["location"]
+
+    # api-endpoint
+    url = 'https://api.yelp.com/v3/businesses/search'
+
+    yelpToken = ""
+
+    # authorization using access token
+    headers = {'Authorization': "Bearer " + yelpToken}
+
+    # parameters I'm passing in to the api request
+    params = {'term': term,
+              'location': location,
+              'open_now': True,
+              'limit': 5
+              }
+    # api-call
+    r = requests.get(url=url, params=params, headers=headers)
+
+    # information passed back from api
+    apiDict = r.json()
+
+    allRestaurants = apiDict["businesses"]
+    restaurantInformation = []
+
+    for restaurant in allRestaurants:
+        name = restaurant["name"]
+        address = restaurant["location"]["display_address"]
+        phone = restaurant["display_phone"]
+        image = restaurant["image_url"]
+        rating = restaurant["rating"]
+
+        restaurantDict = {
+            "name": name,
+            "address": address,
+            "phone": phone,
+            "image": image,
+            "rating": rating
+        }
+
+        restaurantInformation.append(restaurantDict)
+
+    return {"Message": "Success", "restaurants": restaurantInformation}
