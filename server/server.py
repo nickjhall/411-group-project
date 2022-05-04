ba5f47d79ddf2521905fb3a0b379f2de119148fc
nicholasjhall <njhall@bu.edu>
2022-05-03 22:55:24 -0400

    Cleaned up unnecessary code

--- server/server.py
+++ server/server.py
@@ -31,18 +31,6 @@ def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
-@app.route("/")
-def hello_world():
-    print(current_user.is_authenticated)
-    return "<p>This is the CS411 server!</p>"
-
-
-@login_manager.user_loader
-def user_loader(user_id):
-    print("loading user")
-    return User.get_from_id(user_id)
-
-
 @app.route("/login", methods=["GET", "POST"])
 @cross_origin()
 def login():
@@ -66,12 +54,6 @@ def login():
     print(current_user.is_authenticated)
 
     return user.json()
-
-
-@app.route("/logout")
-def logout():
-    print(current_user.is_authenticated)
-    logout_user()
 
 
 @app.route("/findPlans")
@@ -179,25 +161,12 @@ def getBestDates(lat, lon, freeDates):
     for dayWeather in dailyWeather:
         dateWeatherList.append({"date": str(currentDate), "weather": dayWeather["weather"], "temp": dayWeather["temp"]})
         currentDate += timedelta(days=1)
-        print(currentDate)
     
-    # for weather in dateWeatherList:
-        # print(weather["weather"][0])
-        # print(weather["date"].strftime("%Y-%m-%d"))
-        # print(weather["weather"][0]["main"])
-        # print(weather["weather"][0]["main"] == "Clouds")
-        # print("~~~")
+   
     bestDates = []
     selectedDates = [day for day in dateWeatherList if day["date"] in freeDates]
-    print(freeDates)
-    print("Original selectedDates have a length of " + str(len(selectedDates)))
-    print("the selected dates were", selectedDates)
-    print("~~~")
-    print("the dateWeatherList is", dateWeatherList)
-    print("~~~")
-    print("the other print is",dateWeatherList[3]["date"])
 
-    print("checking for best dates that are clear")
+
     for day in selectedDates:
         if day["weather"][0]["main"] == "Clear":
             bestDates.append(day)
@@ -209,14 +178,7 @@ def getBestDates(lat, lon, freeDates):
 
     if len(bestDates) == 0:
         bestDates = selectedDates
-    # bestDates = [day for day in selectedDates if day["weather"][0]["main"] == "Clear"]
-    # print("clear days is " + str(bestDates))
-    # if len(bestDates) == 0:
-    #     print("checking for best dates that are cloudy")
-    #     bestDates = [day for day in selectedDates if day["weather"][0]["main"] == "Clouds"]
-    #     print("cloudy days is " + str(bestDates))
-    # else:
-    #     bestDates = selectedDates
+    
     
     print("bestDates are ", bestDates)
 
@@ -239,113 +201,3 @@ def getPlans():
     plans = Plan.get_from_user_id(createdBy)
     plans_json = jsonify([plan.json() for plan in plans])
     return plans_json
-
-
-@app.route("/findRestaurants")
-@cross_origin()
-def findRestaurants():
-    # term = json["foodType"]
-    # location = json["location"]
-    args = request.args
-    term = args.get("foodType")
-    location = args.get("location")
-
-    # api-endpoint
-    url = 'https://api.yelp.com/v3/businesses/search'
-
-    yelpToken = os.environ.get("YELP_API_KEY")
-
-    # authorization using access token
-    headers = {'Authorization': "Bearer " + yelpToken}
-
-    # parameters I'm passing in to the api request
-    params = {'term': term,
-              'location': location,
-              'limit': 5
-              }
-    # api-call
-    r = requests.get(url=url, params=params, headers=headers)
-
-    # information passed back from api
-    apiDict = r.json()
-
-    allRestaurants = apiDict["businesses"]
-    restaurantInformation = []
-
-    for restaurant in allRestaurants:
-        name = restaurant["name"]
-        address = restaurant["location"]["display_address"]
-        print(address)
-        phone = restaurant["display_phone"]
-        image = restaurant["image_url"]
-        rating = restaurant["rating"]
-        lat = restaurant["coordinates"]["latitude"]
-        lon = restaurant["coordinates"]["longitude"]
-
-        restaurantDict = {
-            "name": name,
-            "address": address,
-            "phone": phone,
-            "image": image,
-            "rating": rating,
-            "lat": lat,
-            "lon": lon
-        }
-
-        restaurantInformation.append(restaurantDict)
-    
-    print(restaurantInformation)
-
-    return {"Message": "Success", "restaurants": restaurantInformation}
-
-
-@app.route("/getWeather")
-@cross_origin()
-def getWeather():
-    args = request.args
-    latitude = args.get("lat")
-    longitude = args.get("lon")
-
-    print(latitude)
-    print(longitude)
-    print(date.today())
-
-    # api-endpoint
-    url = 'https://api.openweathermap.org/data/2.5/onecall'
-
-    weatherToken = os.environ.get("WEATHER_API_KEY")
-
-    # authorization using access token
-    headers = {'Authorization': "Bearer " + weatherToken}
-
-    # parameters I'm passing in to the api request
-    params = {'lat': latitude,
-              'lon': longitude,
-              'units': "imperial",
-              'appid': weatherToken
-              }
-    # api-call
-    r = requests.get(url=url, params=params)
-
-    # information passed back from api
-    apiDict = r.json()
-
-    dailyWeather = apiDict["daily"]
-    weatherInfo = []
-    # print(dailyWeather)
-
-    for day in dailyWeather:
-        print(day["weather"])
-        min = day["temp"]["min"]
-        max = day["temp"]["max"]
-        weather = day["weather"][0]["description"]
-
-        weatherDict = {
-            "min": min,
-            "max": max,
-            "weather": weather
-        }
-
-        weatherInfo.append(weatherDict)
-
-    return {"Message": "Success", "weather": weatherInfo}
